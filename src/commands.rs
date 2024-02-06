5861589807c61875d1c2d9cb664f5ba19309053e
Stepan Kizim <stsiapan.kizim@gmail.com>
2024-02-06 14:19:58 +0200

    Automatically join authors vc if requested to play something

--- src/commands.rs
+++ src/commands.rs
@@ -1,23 +1,23 @@
 use songbird::input::YoutubeDl;
+use tracing::info;
 
 use crate::http_client;
 
 pub(crate) type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, (), Error>;
 
+fn get_author_vc(ctx: &Context<'_>) -> Option<serenity::model::id::ChannelId> {
+    ctx.guild()?
+        .voice_states
+        .get(&ctx.author().id)
+        .and_then(|voice_state| voice_state.channel_id)
+}
+
 /// Join my current voice channel
 #[poise::command(guild_only, slash_command)]
 pub(crate) async fn join(ctx: Context<'_>) -> Result<(), Error> {
-    let (guild_id, channel_id) = {
-        let guild = ctx.guild().unwrap();
-        let channel_id = guild
-            .voice_states
-            .get(&ctx.author().id)
-            .and_then(|voice_state| voice_state.channel_id);
-        (guild.id, channel_id)
-    };
-
-    let Some(channel_id) = channel_id else {
+    let guild_id = ctx.guild().unwrap().id;
+    let Some(channel_id) = get_author_vc(&ctx) else {
         ctx.reply("You should be in a voice channel to invite me")
             .await?;
         return Ok(());
@@ -57,6 +57,7 @@ pub(crate) async fn ping(ctx: Context<'_>) -> Result<(), Error> {
 
 #[poise::command(guild_only, slash_command)]
 pub(crate) async fn play(ctx: Context<'_>, url: String) -> Result<(), Error> {
+    info!("{} requested to play {url}", ctx.author().name);
     let guild_id = ctx.guild().unwrap().id;
 
     if !url.starts_with("http") {
@@ -65,25 +66,31 @@ pub(crate) async fn play(ctx: Context<'_>, url: String) -> Result<(), Error> {
         return Ok(());
     }
 
+    let songbird = songbird::get(ctx.serenity_context())
+        .await
+        .expect("Songbird Voice client placed in at initialisation.");
+
+    let vc = match songbird.get(guild_id) {
+        Some(vc) => vc,
+        None => {
+            let Some(channel_id) = get_author_vc(&ctx) else {
+                ctx.reply("You should be in a voice channel to invite me")
+                    .await?;
+                return Ok(());
+            };
+            songbird.join(guild_id, channel_id).await?
+        }
+    };
+
+    let mut vc = vc.lock().await;
+
+    ctx.reply(format!("Playing {url}")).await?;
+
     let http_client = http_client::get(ctx.serenity_context())
         .await
         .expect("HttpClient should be inserted in at initialisation");
-
-    if let Some(vc) = songbird::get(ctx.serenity_context())
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .get(guild_id)
-    {
-        let mut vc = vc.lock().await;
-
-        ctx.reply(format!("Playing {url}")).await?;
-
-        let src = YoutubeDl::new(http_client, url);
-        let _ = vc.play_only_input(src.into());
-    } else {
-        // todo: join vc message author belongs to
-        ctx.reply("Not in a voice channel to play in").await?;
-    }
+    let src = YoutubeDl::new(http_client, url);
+    let _ = vc.play_only_input(src.into());
 
     Ok(())
 }
