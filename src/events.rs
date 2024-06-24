a5c95a0c12a34a6e53e91b392ad5bf516336e85e
Stepan Kizim <stsiapan.kizim@gmail.com>
2024-06-24 18:58:44 +0300

    feat: Follow the user if they left bot alone in some voice channel

--- src/events.rs
+++ src/events.rs
@@ -5,7 +5,7 @@ use serenity::{
         voice::VoiceState,
     },
 };
-use tracing::{info, warn};
+use tracing::info;
 
 use crate::Data;
 
@@ -53,44 +53,70 @@ pub(crate) async fn voice_state_update(
     old: &Option<VoiceState>,
     new: &VoiceState,
 ) {
-    if let (Some(guild_id), Some(channel_id)) = (new.guild_id, new.channel_id) {
-        if new.user_id == ctx.cache.current_user().id {
-            bot_joined_vc(ctx, data, guild_id, channel_id).await;
-        } else {
-            user_joined_vc(ctx, data, guild_id, channel_id).await;
+    let old_guild_channel = old.as_ref().and_then(|o| o.guild_id.zip(o.channel_id));
+    let new_guild_channel = new.guild_id.zip(new.channel_id);
+    let is_bot = new.user_id == ctx.cache.current_user().id;
+
+    match (old_guild_channel, new_guild_channel) {
+        // Joined voice channel from nowhere
+        (None, Some((guild_id, channel_id))) => {
+            if is_bot {
+                bot_joined_vc(ctx, data, guild_id, channel_id).await;
+            } else {
+                user_joined_vc(ctx, data, guild_id, channel_id).await;
+            }
         }
-    } else if let Some(guild_id) = old.as_ref().and_then(|old| old.guild_id) {
-        if new.user_id == ctx.cache.current_user().id {
-            bot_left_vc(ctx, data, guild_id).await;
-        } else {
-            user_left_vc(ctx, data, guild_id).await;
+        // Moved from one voice channel to another
+        (Some((old_guild_id, old_channel_id)), Some((new_guild_id, new_channel_id)))
+            // This check filters out other events like muting, deafening, etc.
+            if old_guild_id == new_guild_id && old_channel_id != new_channel_id =>
+        {
+            if is_bot {
+                bot_changed_vc(ctx, data, new_guild_id, old_channel_id, new_channel_id).await;
+            } else {
+                user_changed_vc(ctx, data, new_guild_id, old_channel_id, new_channel_id).await;
+            }
         }
+        // Left voice channel
+        (Some((guild_id, _channel_id)), None) => {
+            if is_bot {
+                bot_left_vc(ctx, data, guild_id).await;
+            } else {
+                user_left_vc(ctx, data, guild_id).await;
+            }
+        }
+        // we don't care
+        _ => {}
     }
 }
 
-/// Invoked when bot joined or was moved into a new voice channel
+/// Invoked when bot joined a new voice channel
 async fn bot_joined_vc(ctx: &Context, _data: &Data, guild_id: GuildId, channel_id: ChannelId) {
-    // Bot joined or was moved into a new voice channel, need to setup the vc.
-    // But as we deal with the events, it might happen that we already left vc at the moment
-    // of processing this event, so we should check that `vc` exists in songbird.
-    let Some(vc) = songbird::get(ctx)
-        .await
-        .expect("Songbird Voice client placed in at initialisation.")
-        .get(guild_id)
-    else {
-        warn!("Tried to join voice channel, but it doesn't exist in songbird");
-        return;
-    };
-
     info!(
         "Joined '{}' vc in '{}' guild",
+        &ctx.cache.channel(channel_id).unwrap().name,
         &ctx.cache.guild(guild_id).unwrap().name,
-        &ctx.cache.channel(channel_id).unwrap().name
     );
 
-    // 96k is a default Discord bitrate in guilds without nitro and we pull Spotify with 96k
-    let mut vc = vc.lock().await;
-    vc.set_bitrate(songbird::driver::Bitrate::BitsPerSecond(96_000));
+    setup_vc(ctx, guild_id).await;
+}
+
+/// Invoked when bot changed voice channel either because someone moved it or it moved itself
+async fn bot_changed_vc(
+    ctx: &Context,
+    _data: &Data,
+    guild_id: GuildId,
+    from: ChannelId,
+    to: ChannelId,
+) {
+    info!(
+        "Moved from '{}' vc to '{}' vc in '{}' guild",
+        &ctx.cache.channel(from).unwrap().name,
+        &ctx.cache.channel(to).unwrap().name,
+        &ctx.cache.guild(guild_id).unwrap().name,
+    );
+
+    setup_vc(ctx, guild_id).await;
 }
 
 /// Invoked when bot left voice channel
@@ -117,33 +143,29 @@ async fn user_joined_vc(ctx: &Context, data: &Data, guild_id: GuildId, _channel_
     }
 }
 
+/// Invoked when user changed voice channel
+async fn user_changed_vc(
+    ctx: &Context,
+    _data: &Data,
+    guild_id: GuildId,
+    _from: ChannelId,
+    to: ChannelId,
+) {
+    // If bot left alone in the voice channel, then we should follow the user to the new channel
+    if bot_left_alone(ctx, guild_id) {
+        info!("Bot left alone, following the user to the new vc");
+        let _ = songbird::get(ctx)
+            .await
+            .expect("Songbird Voice client placed in at initialisation.")
+            .join(guild_id, to)
+            .await;
+    }
+}
+
 /// Invoked when user left a voice channel
 async fn user_left_vc(ctx: &Context, _data: &Data, guild_id: GuildId) {
     // Check if bot should leave voice channel when everyone left
-    let bot_left_alone = {
-        let guild = ctx.cache.guild(guild_id).unwrap();
-
-        let bot_channel = guild
-            .voice_states
-            .get(&ctx.cache.current_user().id)
-            .and_then(|voice_state| voice_state.channel_id);
-
-        guild
-            .voice_states
-            .values()
-            .filter(|voice_state| voice_state.channel_id == bot_channel)
-            // Ignore non-members and bots
-            .filter(|voice_state| {
-                voice_state
-                    .member
-                    .as_ref()
-                    .is_some_and(|member| !member.user.bot)
-            })
-            .count()
-            == 0
-    };
-
-    if bot_left_alone {
+    if bot_left_alone(ctx, guild_id) {
         info!("Bot left alone, leaving the vc");
         let _ = songbird::get(ctx)
             .await
@@ -151,4 +173,39 @@ async fn user_left_vc(ctx: &Context, _data: &Data, guild_id: GuildId) {
             .remove(guild_id)
             .await;
     }
+}
+
+async fn setup_vc(ctx: &Context, guild_id: GuildId) {
+    if let Some(vc) = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .get(guild_id)
+    {
+        let mut vc = vc.lock().await;
+        // 96k is a default Discord bitrate in guilds without nitro so no need to send more data
+        vc.set_bitrate(songbird::driver::Bitrate::BitsPerSecond(96_000));
+    }
+}
+
+fn bot_left_alone(ctx: &Context, guild_id: GuildId) -> bool {
+    let guild = ctx.cache.guild(guild_id).unwrap();
+
+    let bot_channel = guild
+        .voice_states
+        .get(&ctx.cache.current_user().id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    guild
+        .voice_states
+        .values()
+        .filter(|voice_state| voice_state.channel_id == bot_channel)
+        // Ignore non-members and bots
+        .filter(|voice_state| {
+            voice_state
+                .member
+                .as_ref()
+                .is_some_and(|member| !member.user.bot)
+        })
+        .count()
+        == 0
 }
