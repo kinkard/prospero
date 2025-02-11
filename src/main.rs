3480ee65f9230a22351f829334bda0543dc509e8
Stepan Kizim <stsiapan.kizim@gmail.com>
2025-02-11 22:53:10 +0100

    perf: Use mimalloc for better performance on alpine

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ mod storage;
 mod track_info;
 mod yt_dlp;
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 struct Data {
     yt_dlp_resolver: yt_dlp::Resolver,
     radio_t_resolver: radiot::Resolver,
